d6f6525688d8fec00d74dfbce0109134d052a894
balde-soul <CJHdream@outlook.com>
2019-12-28 21:08:02 +0800

    building

---
+++ core/base/operation.hpp
@@ -0,0 +1,22 @@
+#ifndef TOY_CORE_BASE_OPERATION_H
+#define TOY_CORE_BASE_OPERATION_H
+#include <core/base/Define.hpp>
+class Operation
+{
+private:
+    /* data */
+public:
+    Operation(/* args */);
+    ~Operation();
+};
+
+Operation::Operation(/* args */)
+{
+}
+
+Operation::~Operation()
+{
+}
+
+#else
+#endif // !1TOY_CORE_BASE_OPERATION_H
